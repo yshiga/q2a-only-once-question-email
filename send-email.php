35d7cc5333abfb629b338ce4ec84e6d46d978a01
yshiga <yuichi.shiga@gmail.com>
2016-06-27 08:13:33 +0900

    small fix

--- send-email.php
+++ send-email.php
@@ -1,5 +1,5 @@
 <?php
-if (!defined('QA_VERSION')) { 
+if (!defined('QA_VERSION')) {
 	require_once dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']).'/../../qa-include/qa-base.php';
 	require_once QA_INCLUDE_DIR.'app/emails.php';
 }
@@ -17,7 +17,7 @@ foreach($posts as $post){
 	$email = $user[0]['email'];
 	$title = "その後ニホンミツバチの質問はありませんか?";
 	$bodyTemplate = qa_opt('q2a-only-once-question-body');
-	$body = strtr($bodyTemplate, 
+	$body = strtr($bodyTemplate,
 		array(
 			'^username' => $handle,
 			'^sitename' => qa_opt('site_title'),
@@ -28,7 +28,6 @@ foreach($posts as $post){
 }
 
 function sendEmail($title, $body, $toname, $toemail){
-
 	$params['fromemail'] = qa_opt('from_email');
 	$params['fromname'] = qa_opt('site_title');
 	$params['subject'] = '【' . qa_opt('site_title') . '】' . $title;
@@ -36,12 +35,9 @@ function sendEmail($title, $body, $toname, $toemail){
 	$params['toname'] = $toname;
 	$params['toemail'] = $toemail;
 	$params['html'] = false;
-	//qa_send_email($params);
-echo $params['toemail'];
-
-	$params['toemail'] = 'yuichi.shiga@gmail.com';
 	qa_send_email($params);
 }
+
 function getUserInfo($userid) {
     $sql = 'select email,handle from qa_users where userid=' . $userid;
     $result = qa_db_query_sub($sql);
